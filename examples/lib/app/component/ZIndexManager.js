94c6d29cbc9b19fc0b204911c1e88316abcca95b
prytula <ruslan.prytula@gmail.com>
2016-08-26 19:54:32 +0300

    up

--- examples/lib/app/component/ZIndexManager.js
+++
@@ -1,8 +0,0 @@
-Ext.define('CJ.component.ZIndexManager', {
-    alternateClassName: 'CJ.ZIndexManager',
-    singleton: true,
-    zIndex: 5,
-    getZIndex() {
-        return ++this.zIndex;
-    }
-});
--- examples/lib/app/view/tablet/nav/SideMenu.js
+++
@@ -1,6 +0,0 @@
-import 'app/view/phone/nav/SideMenu';
-
-Ext.define('CJ.view.tablet.nav.SideMenu', {
-    extend: 'CJ.view.phone.nav.SideMenu',
-    xtype: 'view-tablet-nav-sidemenu'
-});
